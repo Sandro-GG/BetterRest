df75dc396f932285a1922f95b4fbb9547e6788e5
Sandro Gakharia <sandrogakharia@gmail.com>
2025-07-09 19:38:39 +0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sandro Gakharia on 09.07.25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
